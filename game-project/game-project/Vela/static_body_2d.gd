334a8527d8c13c8a0a40097e8e575adf5910e1c8
Aaronprograma537 <aaronaaroncito537@gmail.com>
2026-06-15 14:03:43 -0500

    Delete game-project/game-project directory

--- game-project/game-project/Vela/static_body_2d.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody2D
-
-# Accedemos al nodo de la animación usando su ruta
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
-
-func _ready() -> void:
-	# Le decimos que reproduzca la animación default
-	anim.play("default")
