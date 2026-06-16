e0338dae74ae780b8e7b06cf9c9a9953ad73f563
santirincon401 <santirincon401@gmail.com>
2026-06-15 19:00:09 -0500

    ataque

--- game-project/game-project/Player/character_body_2d.gd
+++ game-project/game-project/Player/character_body_2d.gd
@@ -1,19 +1,41 @@
 extends CharacterBody2D
-@export var speed : float = 300.00
+
+@export var speed : float = 300.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-func _physics_process(delta: float) -> void:
-	var direction : Vector2 = Vector2.ZERO
-	direction.x = Input.get_axis("ui_left", "ui_right")
-	direction.y = Input.get_axis("ui_up", "ui_down")
+
+var atacando = false
+
+func _physics_process(_delta):
+
+	if Input.is_action_just_pressed("ataque") and !atacando:
+		atacando = true
+		animated_sprite.play("ataque")
 	
-	if direction != Vector2.ZERO:
-		direction = direction.normalized()
-		animated_sprite.play("Walk")
-		if direction.x > 0:
-			animated_sprite.flip_h = false 
-		elif direction.x < 0:
-			animated_sprite.flip_h = true 
+	if !atacando:
+		var direction : Vector2 = Vector2.ZERO
+		direction.x = Input.get_axis("ui_left", "ui_right")
+		direction.y = Input.get_axis("ui_up", "ui_down")
+
+		if direction != Vector2.ZERO:
+			direction = direction.normalized()
+			animated_sprite.play("Walk")
+
+			if direction.x > 0:
+				animated_sprite.flip_h = false
+			elif direction.x < 0:
+				animated_sprite.flip_h = true
+		else:
+			animated_sprite.play("default")
+
+		velocity = direction * speed
 	else:
-		animated_sprite.play("default")
-	velocity = direction * speed
+		velocity = Vector2.ZERO
+
 	move_and_slide()
+
+	
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	print("terminó")
+	atacando = false
